e8357c57a67af55d9ed5e7fd666320cea08dcfaf
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-07-05 04:53:14 -0400

    biterators

--- src/main.rs
+++ src/main.rs
@@ -7,93 +7,177 @@ struct BitSlice<'a,ElementType,S> {
     _life: PhantomData<&'a ElementType>
 }
 
-impl <'a,ElementType:BitOps,S:AsRef<[ElementType]>> BitSlice<'a,ElementType,S> {
-    pub fn bounds(&self,bit:usize) {if bit>=self.len() {panic!("Bit: {} is out of bounds as its greater than len: {}",bit,self.len())}}
-    pub fn len(&self) -> usize {(self.slice.as_ref().len()-1)*ElementType::TYPE_BITS+(self.eb-self.sb) as usize}
-    pub fn bits_idx(bit:usize) -> usize {bit/ElementType::TYPE_BITS}
-    pub fn bits_bit(bit:usize) -> usize {bit%ElementType::TYPE_BITS}
-    pub fn get(&self,bit:usize) -> bool {self.slice.as_ref()[Self::bits_idx(bit)].get_bit(Self::bits_bit(bit))}
-    pub fn new(slice:S, sb:usize, eb:usize,) -> Self {Self {slice, sb:sb as u8, eb:eb as u8, _life: PhantomData}}
+pub trait ImmutableBitSlice<ElementType:BitOps, S:AsRef<[ElementType]>>:Sized {
+    fn bounds(&self,bit:usize); 
+    fn len(&self) -> usize;
+    fn bits_idx(bit:usize) -> usize;
+    fn bits_bit(bit:usize) -> usize;
+    fn get(&self,bit:usize) -> bool;
+    fn new(slice:S, sb:usize, eb:usize) -> Self;
+    fn iter<'short>(&'short self) -> Biter<'short,ElementType>;
 }
 
-impl <'a,ElementType:BitOps,S:AsMut<[ElementType]>+AsRef<[ElementType]>> BitSlice<'a,ElementType,S> {
-    pub fn set(&mut self,bit:usize, val:bool) {self.slice.as_mut()[Self::bits_idx(bit)].set_bit(Self::bits_bit(bit),val)}
-    pub fn get_mut<'short>(&'short mut self, bit:usize) -> MutBitProxy<'short,ElementType> {self.slice.as_mut()[Self::bits_idx(bit)].mut_bit(Self::bits_bit(bit))}
+pub trait MutableBitSlice<ElementType:BitOps, S:AsMut<[ElementType]>+AsRef<[ElementType]>> {
+    fn set(&mut self,bit:usize, val:bool);
+    fn get_mut<'short>(&'short mut self, bit:usize) -> MutBitProxy<'short,ElementType>;
+    fn iter_mut<'short>(&'short mut self) -> MutBiter<'short, ElementType>;
 }
 
-// struct Biter<'short,'long,ElementType,S> {
-//     bs: BitSlice<'long,ElementType,S>,
-//     cb: usize,
-//     _blife: PhantomData<&'short &'long ElementType>
-// }
+impl <'a,ElementType:BitOps,S:AsRef<[ElementType]>> ImmutableBitSlice<ElementType,S> for BitSlice<'a,ElementType,S> {
+    fn bounds(&self,bit:usize) {if bit>=self.len() {panic!("Bit: {} is out of bounds as its greater than len: {}",bit,self.len())}}
+    fn len(&self) -> usize {(self.slice.as_ref().len()-1)*ElementType::TYPE_BITS+(self.eb-self.sb) as usize+1}
+    fn bits_idx(bit:usize) -> usize {bit/ElementType::TYPE_BITS}
+    fn bits_bit(bit:usize) -> usize {bit%ElementType::TYPE_BITS}
+    fn get(&self,bit:usize) -> bool {self.slice.as_ref()[Self::bits_idx(bit)].get_bit(Self::bits_bit(bit))}
+    fn new(slice:S, sb:usize, eb:usize,) -> Self {Self {slice, sb:sb as u8, eb:eb as u8, _life: PhantomData}}
+    fn iter<'short>(&'short self) -> Biter<'short,ElementType> {Biter::from(self)}
+}
 
-// struct BiterMut<'short,'long,ElementType,S> {
-//     bs: BitSlice<'long,ElementType,S>,
-//     cb: usize,
-//     _blife: PhantomData<&'short &'long ElementType>
-// }
+impl <'a,ElementType:BitOps,S:AsMut<[ElementType]>+AsRef<[ElementType]>> MutableBitSlice<ElementType, S> for BitSlice<'a,ElementType,S> {
+    fn set(&mut self,bit:usize, val:bool) {self.slice.as_mut()[Self::bits_idx(bit)].set_bit(Self::bits_bit(bit),val)}
+    fn get_mut<'short>(&'short mut self, bit:usize) -> MutBitProxy<'short,ElementType> {self.slice.as_mut()[Self::bits_idx(bit)].mut_bit(Self::bits_bit(bit))}
+    fn iter_mut<'short>(&'short mut self) -> MutBiter<'short,ElementType> {MutBiter::from(self)}
+}
 
-// impl<'short, 'long, ElementType: BitOps, S:AsRef<[ElementType]> + AsMut<[ElementType]>> Iterator for BiterMut<'short, 'long, ElementType, S> {
-//     type Item = MutBitProxy<'short, ElementType>;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if self.cb < self.bs.len() {
-//             self.cb += 1;
-//             // 2. Safely bypass the short &mut self constraint by casting the reference lifetime.
-//             Some(unsafe {std::mem::transmute::<MutBitProxy<'_, ElementType>, MutBitProxy<'short, ElementType>>(self.bs.get_mut(self.cb-1)) })
-//         } else {None}
-//     }
-// }
+// macro_rules! biterators {
+//     ( $( (name: $name:ident, (S: $($s_b:tt)*),ret: $ret:ty, ret_code: |$self_var:ident| $ret_code:expr) ),* ) => {$(
+//         struct $name<'short,'long,ElementType,S> {
+//                 bs: BitSlice<'long,ElementType,S>,
+//                 cb: usize,
+//                 _blife: PhantomData<&'short &'long ElementType>
+//         }
+//         impl<'short, 'long, ElementType: BitOps, S:$($s_b)* > Iterator for $name<'short, 'long, ElementType, S> {
+//             type Item = $ret;
+//             fn next(&mut self) -> Option<Self::Item> {
+//                 if self.cb < self.bs.len() {
+//                     self.cb += 1;
+//                     let $self_var = self; $ret_code
+//                 } else {None}
+//             }
+//         }
 
-// impl <'short,'long,ElementType:BitOps,S:AsRef<[ElementType]>> Iterator for  Biter<'short,'long,ElementType,S> {
-//     type Item=bool;
-//     fn next(&mut self) -> Option<Self::Item> {
-//     if self.cb<self.bs.len() {
-//         self.cb += 1;
-//         Some(self.bs.get(self.cb-1))
-//     } else {None}}
-// }
+//         impl<'short, 'long, ElementType: BitOps, S:$($s_b)* > From<BitSlice<'long,ElementType,S>> for $name<'short,'long,ElementType,S>{
+//             fn from(bs:BitSlice<'long,ElementType,S>) -> Self {Self {bs, cb:0, _blife: PhantomData} }
+//         }
+//     )*}}
 
-macro_rules! biterators {
-    ( $( 
-    (
-        name: $name:ident, 
-        (S: $($s_b:tt)*),
-        ret: $ret:ty, 
-        ret_code: |$self_var:ident| $ret_code:expr 
-    ) 
-    ),* ) => {
-        $(
-            struct $name<'short,'long,ElementType,S> {
-                    bs: BitSlice<'long,ElementType,S>,
-                    cb: usize,
-                    _blife: PhantomData<&'short &'long ElementType>
-            }
-            impl<'short, 'long, ElementType: BitOps, S:$($s_b)* > Iterator for $name<'short, 'long, ElementType, S> {
-                type Item = $ret;
-                fn next(&mut self) -> Option<Self::Item> {
-                    if self.cb < self.bs.len() {
-                        self.cb += 1;
-                        let $self_var = self;
-                        $ret_code
-                    } else {None}
+
+// biterators!(
+//     (name: Biter, (S:AsRef<[ElementType]>),ret: bool, 
+//     ret_code: |this| Some(this.bs.get(this.cb - 1)) )
+//     // ,(name: BiterMut, (S:AsRef<[ElementType]>+AsMut<[ElementType]>),ret: MutBitProxy<'short, ElementType>, 
+//     // ret_code: |this| Some(unsafe {std::mem::transmute::<MutBitProxy<'_, ElementType>, MutBitProxy<'short, ElementType>>(this.bs.get_mut(this.cb-1)) }) )
+
+// );
+
+        // struct Biter<'short,'long,ElementType> {
+        //         cp: *const ElementType,
+        //         rb: usize,
+        //         b:u8,
+        //         _blife: PhantomData<&'short &'long ElementType>,
+        // }
+        // impl<'short, 'long, ElementType: BitOps> Iterator for Biter<'short, 'long, ElementType> {
+        //     type Item = bool;
+        //     fn next(&mut self) -> Option<Self::Item> {
+        //         if self.rb!=0 {
+        //             let bit = unsafe {(*self.cp).get_bit(self.b as usize) };
+        //             self.b+=1;
+        //             self.rb-=1;
+        //             if self.b==ElementType::TYPE_BITS as u8 {
+        //                 self.b=0;
+        //                 unsafe {self.cp = self.cp.add(1)};
+        //             }
+        //             Some(bit)
+        //         } else {None}
+        //     }
+        // }
+
+
+
+        // struct MutBiter<'short,'long,ElementType> {
+        //         cp: *mut ElementType,
+        //         rb: usize,
+        //         b:u8,
+        //         _blife: PhantomData<&'short mut &'long ElementType>,
+        // }
+        // impl<'short, 'long, ElementType: BitOps> Iterator for MutBiter<'short, 'long, ElementType> {
+        //     type Item = MutBitProxy<'short,ElementType>;
+        //     fn next(&mut self) -> Option<Self::Item> {
+        //         if self.rb!=0 {
+        //             let bit = unsafe {(*self.cp).mut_bit(self.b as usize) };
+        //             self.b+=1;
+        //             self.rb-=1;
+        //             if self.b==ElementType::TYPE_BITS as u8 {
+        //                 self.b=0;
+        //                 unsafe {self.cp = self.cp.add(1)};
+        //             }
+        //             Some(bit)
+        //         } else {None}
+        //     }
+        // }
+
+        macro_rules! biterators {
+            (name:$name:ident, item:$item:ty, bit_method:$bit_method:ident, ptr_ty:$ptr_ty:tt  $(, lock:$lock:tt)? ) => {
+                pub struct $name<'short,ElementType> {
+                    cp: *$ptr_ty ElementType,
+                    rb: usize,
+                    b:u8,
+                    _blife: PhantomData<&'short $($lock)? ElementType>,
+                }
+                impl<'short, ElementType: BitOps> Iterator for $name<'short, ElementType> {
+                    type Item = $item;
+                    fn next(&mut self) -> Option<Self::Item> {
+                        if self.rb!=0 {
+                            let bit = unsafe {(*self.cp).$bit_method(self.b as usize) };
+                            self.b+=1;
+                            self.rb-=1;
+                            if self.b==ElementType::TYPE_BITS as u8 {
+                                self.b=0;
+                                unsafe {self.cp = self.cp.add(1)};
+                            }
+                            Some(bit)
+                        } else {None}
+                    }
+                }
+                impl<'short, ElementType: BitOps> $name<'short,ElementType>{
+                    pub fn new(cp:*$ptr_ty ElementType, b:u8, rb:usize) -> Self {Self {cp, b, rb, _blife: PhantomData} }
+                }
+
+                impl <'short,ElementType: BitOps > From<&'short $($lock)? [ElementType]> for $name<'short,ElementType> {
+                    fn from(s:&'short $($lock)? [ElementType]) -> Self {
+                        Self::new(s as *$ptr_ty [ElementType] as *$ptr_ty ElementType,0,s.len()*ElementType::TYPE_BITS)
+                    }
                 }
             }
-        )*
-    }
-}
+        }
+        biterators!(name:Biter,item:bool,bit_method:get_bit, ptr_ty:const);
+        biterators!(name:MutBiter,item:MutBitProxy<'short,ElementType>,bit_method:mut_bit, ptr_ty:mut, lock:mut);
 
-biterators!(
-    (name: Biter, (S:AsRef<[ElementType]>),ret: bool, 
-    ret_code: |this| Some(this.bs.get(this.cb - 1)) ),
-     (name: BiterMut, (S:AsRef<[ElementType]>+AsMut<[ElementType]>),ret: MutBitProxy<'short, ElementType>, 
-     ret_code: |this| Some(unsafe {std::mem::transmute::<MutBitProxy<'_, ElementType>, MutBitProxy<'short, ElementType>>(this.bs.get_mut(this.cb-1)) }) )
+        impl <'short,'long, ElementType: BitOps,S:AsRef<[ElementType]> > From<&'short BitSlice<'long,ElementType,S>> for Biter<'short,ElementType> {
+            fn from(bs:&'short BitSlice<'long,ElementType,S>) -> Self {
+                Self::new(bs.slice.as_ref() as *const [ElementType] as *const ElementType,bs.sb,bs.len())
+            }
+        }
 
-);
+        impl <'short, 'long, ElementType: BitOps,S:AsRef<[ElementType]> + AsMut<[ElementType]>> From<&'short mut BitSlice<'long,ElementType,S>> for MutBiter<'short,ElementType> {
+            fn from(bs:&'short mut BitSlice<'long,ElementType,S>) -> Self {
+                Self::new(bs.slice.as_mut() as *mut [ElementType] as *mut ElementType,bs.sb,bs.len())
+            }
+        }
+
+
+
+
+
+
 fn main() {
     println!("Hello, world!");
-    let slice = &mut [1_u8,2_u8][0..=1];
-    let mut bs = BitSlice::new(slice,0,0);
+    let slice = &mut [1_u8,u8::MAX][0..=1];
+    let mut bs = BitSlice::new(slice,0,7);
     assert_eq!(bs.get(0),true);
     {bs.get_mut(0);}
     assert_eq!(bs.get(1),false);
+    bs.iter_mut().for_each(|mut bit|{println!("{}",*bit);});
+    bs.iter().for_each(|bit|println!("{}",bit));
+    println!("{}",bs.len());
 }
