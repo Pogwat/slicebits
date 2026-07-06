7ba4c800b8ef3dad486d0525ea6b9bcd6fddfd73
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-07-05 21:45:25 -0400

    readme docs

--- README.md
+++ README.md
@@ -6,3 +6,58 @@ Diffrent form version 1.0.0, everything now is in one unified struct BitSlice
 
 ## Examples
 
+Get A bitslice
+```rust
+    use slicebits::BitSlice;
+    let mut array: [u8;2] = [1,2];
+    let immutable_bitslice = BitSlice::new(&array); //Immutable BitSlice
+    let mutable_bitslice = BitSlice::new(&mut array); //Mutable BitSlice
+```
+
+Get And Set bits in a BitSlice
+```rust
+    use slicebits::BitSlice;
+    let mut array: [u8;2] = [1,2];
+    let mut mutable_bitslice = BitSlice::new(&mut array); //Mutable BitSlice
+    assert_eq!(mutable_bitslice.get(0),true); //Immutable and Mutable method
+    mutable_bitslice.set(0,false); //Mutable Method
+    assert_eq!(mutable_bitslice.get(0),false);
+    assert_eq!(array[0],0);
+```
+
+Get A mutable refrence to A bit in a Bitslice (proxy struct). MUST BE DROPPED FOR BIT TO UPDATE. DROP UPDATES!!!!
+```rust
+    use slicebits::BitSlice;
+    let mut array: [u8;2] = [1,2];
+    let mut mutable_bitslice = BitSlice::new(&mut array); //Mutable BitSlice
+    {
+        let mut bit = mutable_bitslice.get_mut(0); //Mutable ref to bit via Deref on proxy struct, updates bit on drop
+        assert_eq!(*bit,true);
+        *bit = false;
+        assert_eq!(*bit,false);
+    } //DROP UPDATES BIT
+    assert_eq!(mutable_bitslice.get(0),false);
+    assert_eq!(array[0],0);
+```
+
+Iterate over bits in a Bitslice
+```rust
+    use slicebits::BitSlice;
+    let array: [u8;2] = [1,2];
+    let bitslice = BitSlice::new(&array);
+    let mut set_bits=0;
+    bitslice.iter().for_each(|bit| set_bits+=bit as usize); //Imutable bools as return, implemented for Immutable and Mutable Bitslices
+    assert_eq!(set_bits,2);
+
+    let mut array = array;
+    set_bits=0;
+    let mut mbitslice = BitSlice::new(&mut array);
+    mbitslice.iter_mut().for_each(|mut bit| *bit = true); //Mutable proxy struct (MutBitProxy) as return, implemented for Mutable Bitslices
+    mbitslice.iter().for_each(|bit| set_bits+=bit as usize);
+    assert_eq!(set_bits,2*8);
+    assert_eq!(bitslice.len(),2*8); //Number of bits in BitSlice, Impl for Mutable and Immutable BitSlices
+```
+
+for full docs use docs.rs : [docs](https://docs.rs/slicebits/latest/slicebits/)
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,58 @@
+#![no_std]
+#[doc = include_str!("../README.md")]
 pub use bit_operations::{BitOps,MutBitProxy};
 pub use biter::{Biter,MutBiter};
 use core::marker::PhantomData;
 /// A BitSlice
 pub struct BitSlice<ElementType,S> {
+    /// The Slice being refenced as bits
     slice:S,
+    /// The start bit
     start_bit:u8,
+    /// The end bit
     end_bit:u8,
     _life: PhantomData<ElementType>
 }
 
+/// Methods for Immutable BitSlice
 impl <ElementType:BitOps,S:AsRef<[ElementType]>> BitSlice<ElementType,S> {
+    ///Check if a git index is in BitSlice, if not panics
     pub fn bounds(&self,bit:usize) {if bit>=self.len() {panic!("Bit: {} is out of bounds as its greater than len: {}",bit,self.len())}}
+    ///Number of bits in BitlSlice
     pub fn len(&self) -> usize {(self.slice.as_ref().len()-1)*ElementType::TYPE_BITS+(self.end_bit-self.start_bit) as usize+1}
+    /// Global Bit Index to bit positon in a element
     pub fn bits_idx(bit:usize) -> usize {bit/ElementType::TYPE_BITS}
+    /// Global Bit Index to a element index
     pub fn bits_bit(bit:usize) -> usize {bit%ElementType::TYPE_BITS}
+    /// Get bit by global index
     pub fn get(&self,bit:usize) -> bool {self.slice.as_ref()[Self::bits_idx(bit)].get_bit(Self::bits_bit(bit))}
+    /// Get a new BitSlice from A type that can be sliced (impls AsRef<[Elementype]>), a start bit, and a end bit (unsafe)
     pub unsafe fn new_uncheked(slice:S, start_bit:usize, end_bit:usize) -> Self {Self {slice, start_bit:start_bit as u8, end_bit:end_bit as u8, _life: PhantomData}}
+    /// Get a new BitSlice from A type that can be sliced (impls AsRef<[Elementype]>)
     pub fn new(slice:S) -> Self {unsafe {Self::new_uncheked(slice, 0, ElementType::TYPE_BITS-1 )}}
+    /// Iterate over a BitSlice, yields bools
     pub fn iter<'short>(&'short self) -> Biter<'short,ElementType> {Biter::from(self)}
 }
 
+/// Methods for Mutable BitSlice
 impl <ElementType:BitOps,S:AsMut<[ElementType]>+AsRef<[ElementType]>> BitSlice<ElementType,S> {
+    /// Set a bit by global index in a BitSlice
     pub fn set(&mut self,bit:usize, val:bool) {self.slice.as_mut()[Self::bits_idx(bit)].set_bit(Self::bits_bit(bit),val)}
+    /// Get Mutable refrence to Bit (porxy struct: MutBitProxy), REF MUST BE DROPPED FOR BIT TO UPDATE. DROP UPDATES!!!
     pub fn get_mut<'short>(&'short mut self, bit:usize) -> MutBitProxy<'short,ElementType> {self.slice.as_mut()[Self::bits_idx(bit)].mut_bit(Self::bits_bit(bit))}
+    /// Mutably Iterate over a BitSlice, yields MutBitProxy that can be Derefed to a bool
     pub fn iter_mut<'short>(&'short mut self) -> MutBiter<'short,ElementType> {MutBiter::from(self)}
 }
 
-impl <'short, ElementType: BitOps,S:AsRef<[ElementType]> > From<&'short BitSlice<ElementType,S>> for Biter<'short,ElementType> {
+///Biter from A Immutable BitSlice ref
+impl <'short,ElementType: BitOps,S:AsRef<[ElementType]> > From<&'short BitSlice<ElementType,S>> for Biter<'short,ElementType> {
     fn from(bs:&'short BitSlice<ElementType,S>) -> Self {
         unsafe {Self::new(bs.slice.as_ref() as *const [ElementType] as *const ElementType,bs.start_bit,bs.len())}
     }
 }
 
-impl <'short,  ElementType: BitOps,S:AsRef<[ElementType]> + AsMut<[ElementType]>> From<&'short mut BitSlice<ElementType,S>> for MutBiter<'short,ElementType> {
+///MutBiter from A Mutable BitSlice ref
+impl <'short,ElementType: BitOps,S:AsRef<[ElementType]> + AsMut<[ElementType]>> From<&'short mut BitSlice<ElementType,S>> for MutBiter<'short,ElementType> {
     fn from(bs:&'short mut BitSlice<ElementType,S>) -> Self {
         unsafe {Self::new(bs.slice.as_mut() as *mut [ElementType] as *mut ElementType,bs.start_bit,bs.len())}
     }
